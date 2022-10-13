51afd1a102ff6f1455630068b123f36f9623b27a
h-kuo <h.kuo@wustl.edu>
2022-10-13 12:46:49 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,36 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		int canvasHeight = 512;
+		int canvasWidth = 22 * canvasHeight / 16;
+		StdDraw.setCanvasSize(canvasWidth, canvasHeight);
 		
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		
+		
+		//StdDraw.setPenColor(255, 0, 0);
+		//StdDraw.filledRectangle(3, 3, 3, 3);
+		//StdDraw.filledRectangle(3, 13, 3, 3);
+		//StdDraw.filledRectangle(16, 3, 6, 3);
+		//StdDraw.filledRectangle(16, 13, 6, 3);
+		
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 5);
+		
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledEllipse(11, 8, 3, 3);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		double[] xCoor = {12, 13, 11, 9, 10};
+		double[] yCoor = {10, 9, 6, 9, 10};
+		StdDraw.filledPolygon(xCoor, yCoor);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.polygon(xCoor, yCoor);
+		StdDraw.line(13, 9, 9, 9);
+		StdDraw.line(12, 10, 11, 9);
+		StdDraw.line(10, 10, 11, 9);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+		boolean filled = in.nextBoolean();
+		
+		if (shape.equals("rectangle")) {
+			if (filled) {
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else if (!filled) {
+				StdDraw.rectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		}
+		
+		if (shape.equals("triangle")) {
+			if (filled) {
+				double[] xCoor = new double[3];
+				double[] yCoor = new double[3];
+				for (int i=0; i<3; i++) {
+					xCoor[i] = in.nextDouble();
+					yCoor[i] = in.nextDouble();
+				}
+				StdDraw.filledPolygon(xCoor, yCoor);
+			}
+			else if (!filled) {
+				double[] xCoor = new double[3];
+				double[] yCoor = new double[3];
+				for (int i=0; i<3; i++) {
+					xCoor[i] = in.nextDouble();
+					yCoor[i] = in.nextDouble();
+				}
+				StdDraw.polygon(xCoor, yCoor);
+			}
+		}
+		
+		if (shape.equals("ellipse")) {
+			if (filled) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else if (!filled) {
+				StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		}
+		
 		
 	}
 }
